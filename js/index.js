2b6c2e99125b3d17be205925cbce2f90f58ada0c
Vitória Garrucho <vitoriagarrucho@gmail.com>
2024-05-14 12:09:27 -0300

    modified:   css/index.css 	modified:   css/style.css 	modified:   index.html 	modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-/*function typeWrite(elemento){
+function typeWrite(elemento){
   const textoArray = elemento.innerHTML.split('');
   elemento.innerHTML = ' ';
   textoArray.forEach(function(letra, i){   
@@ -10,24 +10,4 @@
 });
 }
 const titulo = document.querySelector('.titulo-principal');
-typeWrite(titulo);*/
-
-/* Efeito de escrita no quadrado de diálogo */
-window.onload = function(){
-  var txtTitulo = '* Isso é um titulo';
-  var txt1 = document.getElementById("titulo");
-  
-  var speed = 100;
-  var cont = 0;
-
-  function typeWriter () {
-    if(cont < txtTitulo.length){
-      txt1.innerHTML +=  txtTitulo.charAt(cont) ;
-      cont++;
-      setTimeout(typeWriter, speed);
-     }else{
-       cont = 0;
-     }
-    }
-   typeWriter();
-};
+typeWrite(titulo);
